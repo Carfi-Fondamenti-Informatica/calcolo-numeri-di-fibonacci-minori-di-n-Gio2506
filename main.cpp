fbcf0d3fd21b3746f2edef1ac0501b9c678fb209
Gio2506 <91522962+Gio2506@users.noreply.github.com>
2021-11-11 19:07:09 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
-using namespace std;
+using namespace std ;
+int main(){
+    int n=0,N1=1,N2=1,N3=0;
+    cin>>n;
 
-int main() {
+    if (n>0)
+        {
+        cout << N1 << '\n' << N2 << endl;
+        N3=N1+N2;
+        N1=N2;
+        N2=N3;
+        while (N3<=n){
+        cout << N3 <<endl;
+        N3=N1+N2;
+        N1=N2;
+        N2=N3;
+        }
+    }
    return 0;
+    
 }
